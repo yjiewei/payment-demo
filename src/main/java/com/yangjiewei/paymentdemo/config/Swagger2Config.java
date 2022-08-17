a41493e422d2d73023ef4d54f3b649ebb7447bda
yangjiewei <yang_7131@163.com>
2022-08-17 21:59:16 +0800

    fix:添加swagger文档信息

--- src/main/java/com/yangjiewei/paymentdemo/config/Swagger2Config.java
+++ src/main/java/com/yangjiewei/paymentdemo/config/Swagger2Config.java
@@ -6,6 +6,7 @@ package com.yangjiewei.paymentdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,6 +21,7 @@ public class Swagger2Config {
      */
     @Bean
     public Docket docket() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder().title("微信支付案例接口文档").build()); // 添加文档描述信息，这里只加了标题
     }
 }
