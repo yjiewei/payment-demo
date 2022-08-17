d25fbe201a095741c8fc13a04d5385c4e2e3df60
yangjiewei <yang_7131@163.com>
2022-08-17 22:10:14 +0800

    feat:新增统一响应类

---
+++ src/main/java/com/yangjiewei/paymentdemo/vo/R.java
@@ -0,0 +1,42 @@
+/*
+ * @author yangjiewei
+ * @date 2022/8/17 22:02
+ */
+package com.yangjiewei.paymentdemo.vo;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Accessors(chain = true) // TODO 这个是干嘛用的
+public class R {
+
+    /**
+     * 响应码 响应消息 数据
+     */
+    private Integer code;
+    private String message;
+    private Map<String, Object> data = new HashMap<>();
+
+    public static R ok(){
+        R r = new R();
+        r.setCode(0);
+        r.setMessage("成功");
+        return r;
+    }
+
+    public static R error(){
+        R r = new R();
+        r.setCode(-1);
+        r.setMessage("失败");
+        return r;
+    }
+
+    public R data(String key, Object value){
+        this.data.put(key, value);
+        return this;
+    }
+}
