542480f5470033771653b3bc0f78f83f0cabdcad
yangjiewei <yang_7131@163.com>
2022-08-17 22:01:54 +0800

    fix:@Api是controller描述，@ApiOperation是接口方法描述

--- src/main/java/com/yangjiewei/paymentdemo/controller/ProductController.java
+++ src/main/java/com/yangjiewei/paymentdemo/controller/ProductController.java
@@ -4,10 +4,13 @@
  */
 package com.yangjiewei.paymentdemo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "商品管理")
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
@@ -15,6 +18,7 @@ public class ProductController {
     /**
      * http://localhost:8090/api/product/test
      */
+    @ApiOperation("测试接口")
     @GetMapping("/test")
     public String test() {
         return "hello";
