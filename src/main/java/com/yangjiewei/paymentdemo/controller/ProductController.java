a21f1a3e690c55d5c92b7e141460ef6228326d1d
yangjiewei <yang_7131@163.com>
2022-08-20 23:15:50 +0800

    feat:新增获取商品列表接口

--- src/main/java/com/yangjiewei/paymentdemo/controller/ProductController.java
+++ src/main/java/com/yangjiewei/paymentdemo/controller/ProductController.java
@@ -4,6 +4,8 @@
  */
 package com.yangjiewei.paymentdemo.controller;
 
+import com.yangjiewei.paymentdemo.entity.Product;
+import com.yangjiewei.paymentdemo.service.ProductService;
 import com.yangjiewei.paymentdemo.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,12 +13,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @Api(tags = "商品管理")
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
+
+    @Resource
+    private ProductService productService;
 
     /**
      * http://localhost:8090/api/product/test
@@ -28,4 +35,13 @@ public class ProductController {
         return R.ok().data("数据", "成功").data("时间", new Date());
     }
 
+    /**
+     * 获取商品列表
+     */
+    @ApiOperation("获取商品列表接口")
+    @GetMapping("/getProductList")
+    public R getProductList() {
+        List<Product> productList = productService.list();
+        return R.ok().data("商品列表", productList);
+    }
 }
