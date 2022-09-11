5aac1ce43fa9ee116e5ef377d650cbb660caf8c9
yangjiewei <yang_7131@163.com>
2022-09-11 12:14:15 +0800

    doc:新增支付宝文档说明

--- src/main/java/com/yangjiewei/paymentdemo/config/AlipayClientConfig.java
+++ src/main/java/com/yangjiewei/paymentdemo/config/AlipayClientConfig.java
@@ -4,8 +4,13 @@
  */
 package com.yangjiewei.paymentdemo.config;
 
+import com.alipay.api.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.Resource;
 
 /**
  *  // 通过spring的环境配置去获取对应的参数
@@ -15,5 +20,30 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource("classpath:alipay-sandbox.properties")
 public class AlipayClientConfig {
+
+    @Resource
+    private Environment env;
+
+    @Bean
+    public AlipayClient alipayClient() throws AlipayApiException {
+
+        AlipayConfig alipayConfig = new AlipayConfig();
+        //设置网关地址
+        alipayConfig.setServerUrl(env.getProperty("alipay.gateway-url"));
+        //设置应用ID
+        alipayConfig.setAppId(env.getProperty("alipay.app-id"));
+        //设置应用私钥
+        alipayConfig.setPrivateKey(env.getProperty("alipay.merchant-private-key"));
+        //设置请求格式，固定值json
+        alipayConfig.setFormat(AlipayConstants.FORMAT_JSON);
+        //设置字符集
+        alipayConfig.setCharset(AlipayConstants.CHARSET_UTF8);
+        //设置支付宝公钥
+        alipayConfig.setAlipayPublicKey(env.getProperty("alipay.alipay-public-key"));
+        //设置签名类型
+        alipayConfig.setSignType(AlipayConstants.SIGN_TYPE_RSA2);
+        //构造client
+        return new DefaultAlipayClient(alipayConfig);
+    }
 
 }
